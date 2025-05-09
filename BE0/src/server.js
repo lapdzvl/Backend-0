7a2ea88708f89056058568e10e65eb6c5d4a0e7a
lappp <lap01288399558@gmail.com>
2025-05-10 01:08:27 +0700

    9.5.25

--- BE0/src/server.js
+++ BE0/src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 console.log(">>>check env: ", process.env);
 const app = express(); //app express
@@ -15,6 +16,27 @@ configViewEngine(app);
 // khai ba'o route
 app.use("/", webRoutes);
 
+//test connection
+
+// Create the connection to database
+
+async function main() {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "123456",
+    database: "hoidanit",
+  });
+
+  connection.query("select * from Users", function (err, results, fields) {
+    console.log(">>>Results:", results);
+    console.log(">>>fielsd:", fields);
+  });
+}
+main().catch((err) => {
+  console.error(">>> Error:", err);
+});
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
